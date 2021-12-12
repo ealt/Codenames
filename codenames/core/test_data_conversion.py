0ee033408c92e5321b4993d6f174a22e1b60b570
Eric Alt <ericallenalt@gmail.com>
2021-12-11 17:59:37 -0800

    [feat] Create dict-to-pb converter for clues
    #9

--- codenames/core/test_data_conversion.py
+++ codenames/core/test_data_conversion.py
@@ -1,8 +1,11 @@
 from typing import Optional
 
-from codenames.core.codenames_pb2 import (CommonInformation, SecretInformation)
+from codenames.core.codenames_pb2 import (Clue, CommonInformation,
+                                          SecretInformation, SharedClue)
 
-from codenames.core.types import (AgentDict, Codename, DictData, UnknownTeam)
+from codenames.core.types import (AgentDict, Codename, DictData, TeamClueDict,
+                                  TeamDictClueDict, TeamSharedClueDict,
+                                  UnknownTeam, Unlimited)
 from codenames.core.test_data import TestData
 
 
@@ -16,7 +19,15 @@ def convert_to_pb(dict_data: DictData) -> TestData:
         all_codenames: Optional[set[Codename]] = None
         secret_information: SecretInformation = {}
         common_information: CommonInformation = {}
-    return TestData(secret_information, common_information, {}, {}, {}, {})
+    if 'clues' in dict_data:
+        dict_clues = dict_data['clues']
+        clues = _get_team_clue_dict(dict_clues)
+        shared_clues = _get_team_shared_clue_dict(clues)
+    else:
+        clues: TeamClueDict = {}
+        shared_clues: TeamSharedClueDict = {}
+    return TestData(secret_information, common_information, clues, shared_clues,
+                    {}, {})
 
 
 def _get_secret_information(agents: AgentDict) -> SecretInformation:
@@ -43,3 +54,20 @@ def _get_all_codenames(agents: AgentDict) -> list[Codename]:
             raise ValueError('codenames must be unique')
         all_codenames |= team_codenames
     return all_codenames
+
+
+def _get_team_clue_dict(clues: TeamDictClueDict) -> TeamClueDict:
+    return {
+        team: [
+            Clue(word=clue['word'], unlimited=True) if clue['quantity']
+            == Unlimited else Clue(word=clue['word'], number=clue['quantity'])
+            for clue in team_clues
+        ] for team, team_clues in clues.items()
+    }
+
+
+def _get_team_shared_clue_dict(clues: TeamClueDict) -> TeamSharedClueDict:
+    return {
+        team: [SharedClue(team=team, clue=clue) for clue in team_clues
+              ] for team, team_clues in clues.items()
+    }
