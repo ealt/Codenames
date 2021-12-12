f2ee402ed09759cbff54d1125d35cf7a03eaf057
Eric Alt <ericallenalt@gmail.com>
2021-12-11 17:38:12 -0800

    [feat] Create dataclass for test data
    resolves #6

---
+++ codenames/core/test_data.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+from codenames.core.codenames_pb2 import CommonInformation, SecretInformation
+from codenames.core.types import (TeamActionDict, TeamClueDict,
+                                  TeamSharedActionDict, TeamSharedClueDict)
+
+
+@dataclass
+class TestData:
+    secret_information: SecretInformation
+    common_information: CommonInformation
+    clues: TeamClueDict
+    shared_clues: TeamSharedClueDict
+    actions: TeamActionDict
+    shared_actions: TeamSharedActionDict
--- codenames/core/types.py
+++ codenames/core/types.py
@@ -1,4 +1,13 @@
 from typing import NewType
 
+from codenames.core.codenames_pb2 import Action, Clue, SharedAction, SharedClue
+
 Team = NewType('Team', int)
 UnknownTeam = 0
+
+# TestData Types
+TeamClueDict = NewType('TeamClueDict', dict[Team, list[Clue]])
+TeamSharedClueDict = NewType('TeamSharedClueDict', dict[Team, list[SharedClue]])
+TeamActionDict = NewType('TeamActionDict', dict[Team, list[Action]])
+TeamSharedActionDict = NewType('TeamSharedActionDict', dict[Team,
+                                                            list[SharedAction]])
