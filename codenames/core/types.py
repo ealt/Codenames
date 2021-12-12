3525c7d97bcf7b7b59512883bea25ff0d02fe985
Eric Alt <ericallenalt@gmail.com>
2021-12-11 17:44:46 -0800

    [feat] Definte json test data schema
    resolves #8

--- codenames/core/types.py
+++ codenames/core/types.py
@@ -4,10 +4,36 @@ from codenames.core.codenames_pb2 import Action, Clue, SharedAction, SharedClue
 
 Team = NewType('Team', int)
 UnknownTeam = 0
+Codename = NewType('Codename', str)
 
-# TestData Types
+# protobuf based test data types
 TeamClueDict = NewType('TeamClueDict', dict[Team, list[Clue]])
 TeamSharedClueDict = NewType('TeamSharedClueDict', dict[Team, list[SharedClue]])
 TeamActionDict = NewType('TeamActionDict', dict[Team, list[Action]])
 TeamSharedActionDict = NewType('TeamSharedActionDict', dict[Team,
                                                             list[SharedAction]])
+
+# json based test data types
+"""
+DictClue: {
+    'word': str,
+    'quantity': int | Unlimited
+}
+
+StrAction: Codename | Pass
+
+DictData: {
+    'agents': AgentDict,
+    'clues': TeamDictClueDict,
+    'actions': TeamStrActionDict
+}
+"""
+AgentDict = dict[Team, list[Codename]]
+Unlimited = 'unlimited'
+DictClue = NewType('DictClue', dict[str, str | int])
+TeamDictClueDict = NewType('TeamDictClueDict', dict[Team, list[DictClue]])
+Pass = 'pass_'
+StrAction = NewType('DictAction', str)
+TeamStrActionDict = NewType('TeamStrActionDict', dict[Team, list[StrAction]])
+DictData = NewType('DictData',
+                   dict[str, AgentDict | TeamDictClueDict | TeamStrActionDict])
