2fefe29c3f1106d3261a818980b2c6d5abb45f17
Eric Alt <ericallenalt@gmail.com>
2021-12-12 16:22:05 -0800

    [style] Change team type from int to Team

--- codenames/players/preprogrammed_codemaster.py
+++ codenames/players/preprogrammed_codemaster.py
@@ -1,10 +1,11 @@
 from codenames.data.codenames_pb2 import Clue, CommonInformation, SecretInformation
+from codenames.data.types import Team
 from codenames.players.codemaster import Codemaster
 
 
 class PreprogrammedCodemaster(Codemaster):
 
-    def set_up(self, team: int, common_information: CommonInformation,
+    def set_up(self, team: Team, common_information: CommonInformation,
                secret_information: SecretInformation,
                clues: list[Clue]) -> None:
         super().set_up(team, common_information, secret_information)
--- codenames/players/preprogrammed_interpreter.py
+++ codenames/players/preprogrammed_interpreter.py
@@ -1,10 +1,11 @@
 from codenames.data.codenames_pb2 import Action, CommonInformation
+from codenames.data.types import Team
 from codenames.players.interpreter import Interpreter
 
 
 class PreprogrammedInterpreter(Interpreter):
 
-    def set_up(self, team: int, common_information: CommonInformation,
+    def set_up(self, team: Team, common_information: CommonInformation,
                actions: list[Action]) -> None:
         super().set_up(team, common_information)
         self._actions = list(reversed(actions))
