236a1d61970e4da4a5eb1bc4b8b90703b3fff46c
Eric Alt <ericallenalt@gmail.com>
2021-12-17 11:02:29 -0800

    [feat] Implement PlayerTeams methods
    #16

--- codenames/game/player_teams.py
+++ codenames/game/player_teams.py
@@ -1,3 +1,4 @@
+from typing import Iterator
 from codenames.data.types import NullTeam, Team
 from codenames.game.player_team import PlayerTeam
 
@@ -5,6 +6,7 @@ from codenames.game.player_team import PlayerTeam
 class PlayerTeams:
 
     def __init__(self, teams: list[Team]) -> None:
+        self.__len = len(teams)
         self._active = PlayerTeam(NullTeam)
         if teams:
             self._init_cyclic_doubly_linked_list(teams)
@@ -23,3 +25,38 @@ class PlayerTeams:
     @property
     def active_team(self) -> Team:
         return self._active.team
+
+    def __len__(self) -> int:
+        return self.__len
+
+    def __getitem__(self, team: Team) -> PlayerTeam:
+        curr = self._active
+        while curr.team != team:
+            curr = curr.next
+            if curr == self._active:
+                raise KeyError
+        return curr
+
+    def __delitem__(self, team: Team) -> None:
+        node = self[team]
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        if node == self._active:
+            self._active = node.next
+        self.__len -= 1
+        if len(self) == 0:
+            self._active = PlayerTeam(NullTeam)
+
+    def __iter__(self) -> Iterator[Team]:
+        return self
+
+    def __next__(self) -> Team:
+        self._active = self._active.next
+        return self.active_team
+
+    def __contains__(self, team: Team) -> bool:
+        try:
+            _ = self[team]
+        except KeyError:
+            return False
+        return True
