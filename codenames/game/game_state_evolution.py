3570feb63025bbc766c92be8a5e18c4462644364
Eric Alt <ericallenalt@gmail.com>
2021-12-16 18:52:26 -0800

    [feat] Initialize end turn resolution
    #16

--- codenames/game/game_state_evolution.py
+++ codenames/game/game_state_evolution.py
@@ -1,8 +1,19 @@
-from codenames.data.codenames_pb2 import Clue, Role
-from codenames.data.types import Quantity
+from codenames.data.codenames_pb2 import Action, Clue, Role
+from codenames.data.types import EndTurn, Quantity, Team
 from codenames.game.game_state import GameState
 
 
 def resolve_clue(game_state: GameState, clue: Clue) -> None:
     game_state.guesses_remaining = Quantity(clue.quantity)
     game_state.active_role = Role.INTERPRETER
+
+
+def resolve_action(game_state: GameState, action: Action) -> None:
+    if action.guess == EndTurn:
+        _end_turn(game_state)
+
+
+def _end_turn(game_state: GameState) -> None:
+    game_state.active_team = Team(0)
+    game_state.active_role = Role.CODEMASTER
+    game_state.guesses_remaining = Quantity(0)
