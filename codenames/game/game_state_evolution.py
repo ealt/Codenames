db76ae9dc1d6b65c7a8e48b5a1cef5253f76e495
Eric Alt <ericallenalt@gmail.com>
2021-12-16 18:54:34 -0800

    [feat] Get next team
    #16

--- codenames/game/game_state_evolution.py
+++ codenames/game/game_state_evolution.py
@@ -14,6 +14,14 @@ def resolve_action(game_state: GameState, action: Action) -> None:
 
 
 def _end_turn(game_state: GameState) -> None:
-    game_state.active_team = Team(0)
+    game_state.active_team = _get_next_team(game_state)
     game_state.active_role = Role.CODEMASTER
     game_state.guesses_remaining = Quantity(0)
+
+
+def _get_next_team(game_state: GameState) -> Team:
+    teams = game_state.team_outcomes.undetermined
+    n_teams = len(teams)
+    current_index = teams.index(game_state.active_team)
+    next_index = (current_index + 1) % n_teams
+    return Team(teams(next_index))
