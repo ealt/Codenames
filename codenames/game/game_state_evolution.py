8ae14eed7d2b32036404f90353b1c9a55e04bb41
Eric Alt <ericallenalt@gmail.com>
2021-12-17 17:12:57 -0800

    [feat] Init clue and action validation
    #16

--- codenames/game/game_state_evolution.py
+++ codenames/game/game_state_evolution.py
@@ -3,14 +3,19 @@ from codenames.data.types import (
     AssassinTeam, Codename, EndTurn, NonPlayerTeams, Quantity, Team, Unlimited
 )
 from codenames.game.game_state import GameState
+from codenames.game.game_validation import validate_action, validate_clue
 
 
 def resolve_clue(game_state: GameState, clue: Clue) -> None:
+    if not validate_clue(clue):
+        raise ValueError
     game_state.guesses_remaining = Quantity(clue.quantity)
     game_state.active_role = Role.INTERPRETER
 
 
 def resolve_action(game_state: GameState, action: Action) -> None:
+    if not validate_action(action):
+        raise ValueError
     if action.guess == EndTurn:
         _end_turn(game_state)
     else:
---
+++ codenames/game/game_validation.py
@@ -0,0 +1,9 @@
+from codenames.data.codenames_pb2 import Action, Clue
+
+
+def validate_clue(clue: Clue) -> bool:
+    return True
+
+
+def validate_action(action: Action) -> bool:
+    return True
