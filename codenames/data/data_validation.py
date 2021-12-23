84bac4f500f71535b6b27ee51ce264e9b04a7382
Eric Alt <ericallenalt@gmail.com>
2021-12-23 00:47:19 -0600

    [refactor] Remove obsolete methods

--- codenames/data/data_validation.py
+++
@@ -1,8 +0,0 @@
-from codenames.data.types import NonPlayerTeams, Team
-
-
-def validate_teams(teams: set[Team]) -> bool:
-    n_teams = max(teams)
-    player_teams = set(range(1, n_teams + 1))
-    all_teams = player_teams | NonPlayerTeams
-    return teams.issuperset(player_teams) and teams.issubset(all_teams)
--- codenames/data/utils.py
+++ codenames/data/utils.py
@@ -3,10 +3,7 @@ from typing import Optional
 from codenames.data.codenames_pb2 import (
     CommonInformation, SharedAction, SharedClue, Turn
 )
-from codenames.data.data_validation import validate_teams
-from codenames.data.types import (
-    Codename, NullTeam, Quantity, Team, UnknownTeam, Unlimited
-)
+from codenames.data.types import Codename, NullTeam, Team, UnknownTeam
 
 
 def update_information_with_clue(
@@ -50,37 +47,3 @@ def get_last_action(
         return common_information.turn_history[-1].actions[-1]
     except IndexError:
         return None
-
-
-def get_n_teams(common_information: CommonInformation) -> int:
-    teams = (
-        set(common_information.identity_counts)
-        | set(common_information.agent_sets)
-    )
-    if not validate_teams(teams):
-        raise ValueError
-    return max(teams)
-
-
-def get_guesses_remaining(common_information: CommonInformation) -> Quantity:
-    last_clue = get_last_clue(common_information)
-    if last_clue is None:
-        return Quantity(0)
-    clue_guesses = last_clue.quantity
-    if clue_guesses == Unlimited:
-        return Unlimited
-    guesses_made = len(common_information.turn_history[-1].actions)
-    return Quantity(clue_guesses - guesses_made)
-
-
-def get_active_team(common_information: CommonInformation) -> Team:
-    last_clue = get_last_clue(common_information)
-    if last_clue is None or last_clue.team in (NullTeam, UnknownTeam):
-        return UnknownTeam
-    last_active_team = last_clue.team
-    guesses_remaining = get_guesses_remaining(common_information)
-    if guesses_remaining == 0:
-        n_teams = get_n_teams(common_information)
-        return ((last_active_team + 1) % n_teams) + 1
-    else:
-        return last_active_team
