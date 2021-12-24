ad322a2f870a2db311a26132734b45942e0e493b
Eric Alt <ericallenalt@gmail.com>
2021-12-23 22:12:12 -0600

    [style] Update formatting in codemaster

--- codenames/players/codemaster.py
+++ codenames/players/codemaster.py
@@ -8,11 +8,14 @@ class Codemaster(Player):
 
     @classmethod
     def __subclasshook__(cls, subclass):
-        return (hasattr(subclass, 'give_clue') and
-                (callable(subclass.give_clue) or NotImplemented))
+        return (
+            hasattr(subclass, 'give_clue')
+            and (callable(subclass.give_clue) or NotImplemented)
+        )
 
     def reaveal_secret_information(
-            self, secret_information: SecretInformation) -> None:
+        self, secret_information: SecretInformation
+    ) -> None:
         self._secret_information = secret_information
 
     @abc.abstractmethod
